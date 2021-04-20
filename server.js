5e534dd48de8334bfe11e4b99093b982a5732160
Nidhi Bhanushali <bhanushalinidhi44@gmail.com>
2021-04-20 10:31:10 +0530

    Basic express server setup

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require ('express');
+
+const app = express();
+
+app.use(express.json({ extended: false }));
+
+app.get('/' , (req , res) => 
+    res.json({msg: 'Welcome to the News Application API'})
+);
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT , () => console.log(`Server started at port ${PORT}`))
