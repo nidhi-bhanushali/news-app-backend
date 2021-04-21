d283630d5469a141795523fc8f07d085a6eab98e
Nidhi Bhanushali <bhanushalinidhi44@gmail.com>
2021-04-21 19:21:53 +0530

    Prepare for deploy

--- server.js
+++ server.js
@@ -9,9 +9,6 @@ connectDB();
 // Init middleware
 app.use(express.json({ extended: false }));
 
-app.get('/' , (req , res) => 
-    res.json({msg: 'Welcome to the News Application API'})
-);
 
 // Define routes
 app.use('/api/users' , require('./routes/users'))
